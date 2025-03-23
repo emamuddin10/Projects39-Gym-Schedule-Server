658f8943aef9c13aafc5e2235f0a89af1fdad22c
Emam Uddin <emamskingdom@gmail.com>
2025-03-23 22:37:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// seassion
+// bbU3s9G9g61b6xFT
+
+
+const uri = "mongodb+srv://seassion:bbU3s9G9g61b6xFT@cluster0.v28xn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('the appcation is runnig')
+})
+app.post('/addData',async(req,res)=>{
+    const data = await req.body
+    console.log(data)
+    // res.send('data paici vai ',data)
+})
+
+app.listen(port,()=>{
+    console.log(`the local server is runing is port: ${port}`)
+})
